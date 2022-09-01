1122c70a771035a54e36ddabf68c3678a8a7e4e9
Amit P <aspper20@gmail.com>
2022-09-02 03:42:38 +0530

    added frontend build as homepage

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Sample React Build to be served by Fast API</h1>
+    </div>
+  );
+}
+
+export default App;
--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from todo import todo_router
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="client/build/static"), name="static")
+
+templates = Jinja2Templates(directory="client/build")
+
+@app.get("/home")
+async def serve_spa(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 @app.get("/")
 async def welcome():
