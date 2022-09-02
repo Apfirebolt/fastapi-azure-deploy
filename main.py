104326e005e5b9ce61d7f7a5fb959addcd7bc8c5
Amit P <aspper20@gmail.com>
2022-09-03 03:21:06 +0530

    added default route capture

--- main.py
+++ main.py
@@ -20,8 +20,12 @@ async def startup():
 async def shutdown():
     print('ended now db')
 
-@app.get("/")
-async def serve_spa(request: Request):
+@app.get("/{full_path:path}")
+async def serve_react_app(request: Request, full_path: str):
+    """Serve the react app
+    `full_path` variable is necessary to serve each possible endpoint with
+    `index.html` file in order to be compatible with `react-router-dom
+    """
     return templates.TemplateResponse("index.html", {"request": request})
     
 
