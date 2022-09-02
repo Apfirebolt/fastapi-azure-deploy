e287f316f66c1bddf29725ea49b4ec2bfa27b376
Amit P <aspper20@gmail.com>
2022-09-02 23:46:44 +0530

    changed startup file again

--- startup.sh
+++ startup.sh
@@ -1,1 +1,7 @@
+# <module-path> is the relative path to the folder that contains the module
+# that contains wsgi.py; <module> is the name of the folder containing wsgi.py.
 gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app
+alembic revision --autogenerate -m "Initial tables"
+alembic upgrade head
+
+
